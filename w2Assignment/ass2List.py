25bd2d1024f4aa84a30793d390f319a7bded0ecd
sacridlamar <sacridlamar91@gmail.com>
2025-07-26 15:04:11 +0300

    Reorganized into folders & added Ass.2

---
+++ w2Assignment/ass2List.py
@@ -0,0 +1,26 @@
+#1.Create an empty list called my_list.
+my_list = []
+
+#2.Append the following elements to my_list: 10, 20, 30, 40.
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#3.Insert the value 15 at the second position in the list.
+my_list.insert(1,15)
+
+#print(my_list)
+
+#4.Extend my_list with another list: [50, 60, 70].
+my_list.extend([50, 60, 70])
+
+#5.Remove the last element from my_list.
+#Current list => [10, 15, 20, 30, 40, 50, 60, 70]
+del my_list[-1]
+
+#6.Sort my_list in ascending order.
+my_list.sort()
+
+#Find and print the index of the value 30 in my_list.
+print(my_list[3])
